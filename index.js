47f041fa8a7ae3aa41377f7540b1cb2b0df051bc
antoniorusso2 <russoantonio303@outlook.it>
2024-11-14 16:31:43 +0100

    -tested watch; -made some tests about changing values in the js script from terminal

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log("Hello World");
+
+const params = process.argv; //process => obj /// argv => key
+const node = params[0];
+const filePath = params[1];
+
+// console.log(params);
+console.log(node);
+console.log(filePath);
+
+const myWord = params[2];
+
+console.log("Hello", myWord);
+console.log(`Hello ${myWord}`);
